e0f22097d938404642f63d211aabe76f766aecc4
gman21788 <69609207+gman21788@users.noreply.github.com>
2021-09-22 19:02:04 -0400

    Update fizzbuzz0.js

--- scripts/fizzbuzz0.js
+++ scripts/fizzbuzz0.js
@@ -1,10 +1,25 @@
 function getVars() {
 	let firstName = document.getElementById('firstName').value;
+	let middleInit = document.getElementById('middleInit').value;	
 	let lastName = document.getElementById('lastName').value;
-	let middleInit = document.getElementById('middleInit').value;
 	document.getElementById('greeting').textContent = 'Welcome to the Crab Shack, ' + firstName + ' ' + middleInit + ' ' + lastName + '!';
-	
+
 	let counter = prompt('How many lines are you looking to buy ${firstName}?');
 	fizzBuzz(counter);	
 	}
 
+funtion fizzBuzz(counter) {
+	for(let i = 0; i <= counter; i++ {
+		var num;
+		if (i % 2 === 0) {
+			num = 'the number is even';
+		} else {
+			num = 'the number is odd';
+		}
+		var listElement = document.createElement('li');
+		var textNode = document.createTextNode(i + ' Hot & Heavy ' + num;
+		listElement.appendChild(textNode);
+		var findPosition = document.getElementsByTagName('ul')[0];
+		findPosition.appendChild(listElement);
+	}
+}
