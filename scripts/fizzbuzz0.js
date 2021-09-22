d23ccd2f060ccb8db1cc060d0224cb774fccc6a9
gman21788 <69609207+gman21788@users.noreply.github.com>
2021-09-21 20:28:53 -0400

    Update fizzbuzz0.js

--- scripts/fizzbuzz0.js
+++ scripts/fizzbuzz0.js
@@ -3,7 +3,8 @@ function getVars() {
 	let lastName = document.getElementById('lastName').value;
 	let middleInit = document.getElementById('middleInit').value;
 	document.getElementById('greeting').textContent = 'Welcome to the Crab Shack, ' + firstName + ' ' + middleInit + ' ' + lastName + '!';
+	
+	let counter = prompt('How many lines are you looking to buy ${firstName}?');
+	fizzBuzz(counter);	
 	}
 
-let counter = prompt('How many lines are you looking to buy ${firstName}?');
-fizzBuzz(counter);
