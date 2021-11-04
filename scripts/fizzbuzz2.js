1ab5d0bf262e7297268b36e1d6e80114ffc976a7
gman21788 <69609207+gman21788@users.noreply.github.com>
2021-11-04 19:09:37 -0400

    Update fizzbuzz2.js

--- scripts/fizzbuzz2.js
+++ scripts/fizzbuzz2.js
@@ -4,50 +4,36 @@ function getVars() {
 	let lastName = document.getElementById('lastName').value;
 	let firstDivisor = 3;
 	let secondDivisor = 5;
-	
 	document.getElementById('greeting').textContent = 'Welcome to the Crab Shack, ' + firstName + ' ' + middleInit + ' ' + lastName + '!';
-
 	fizzBuzz(firstDivisor, secondDivisor);	
 	}
 
 function isDivisible(firstDivisor, secondDivisor) {
 	if (firstDivisor % secondDivisor === 0) {
 		return true;
-	} 
-	else {
+	} else {
 		return false;
 	}
 }
 
 
 function fizzBuzz(firstDivisor, secondDivisor) {
-   for(let i = 1; i <= 140; i++) {
-     var word = '';
-  
-     if (i % (firstDivisor * secondDivisor) === 0) {
-       word = word.concat('HotHEAVY');
-     }
-     else if (i % firstDivisor === 0) {
-       word = word.concat('Hot');
-     }
-     else if (i % secondDivisor === 0) {
-       word = word.concat('HEAVY');
-     }
-     else {
-       word = word.concat('Order Crabs!');
-     }
+   	for(let i = 1; i <= 140; i++) {
+     		var word = '';  
+     		if (i % (firstDivisor * secondDivisor) === 0) {
+       		word = word.concat('HotHEAVY');
+     		} else if (i % firstDivisor === 0) {
+       		word = word.concat('Hot');
+     		} else if (i % secondDivisor === 0) {
+       		word = word.concat('HEAVY');
+		} else {
+       		word = word.concat('Order Crabs!');
+     	}
 
-
-    var element1 = document.createElement('li');
-
-    var newText = document.createTextNode(i + ' ' + word);
-    
-    element1.appendChild(newText);
-    
-    var pos_new = document.getElementsByTagName('ol')[0];
-    
-    pos_new.appendChild(element1);
-    
-  }
-  
+	var element1 = document.createElement('li');
+	var newText = document.createTextNode(' ' + word);   
+	element1.appendChild(newText);    
+	var pos_new = document.getElementsByTagName('ol')[0];  
+	pos_new.appendChild(element1);  
+  	}  
 }
