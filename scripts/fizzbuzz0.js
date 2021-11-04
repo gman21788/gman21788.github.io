fb06df6174caa3338ba4f530911f2210b02150ae
gman21788 <69609207+gman21788@users.noreply.github.com>
2021-11-04 18:34:51 -0400

    Update fizzbuzz0.js

--- scripts/fizzbuzz0.js
+++ scripts/fizzbuzz0.js
@@ -18,9 +18,9 @@ for(let i = 0; i <= lineCounter; i++) {
     evenOdd = 'this is an odd number';
   }
 
-    var element1 = document.createElement('ol');
+    var element1 = document.createElement('li');
 
-    var newText = document.createTextNode(' Hot & Heavy ' + evenOdd);
+    var newText = document.createTextNode(i + ' Hot & Heavy ' + evenOdd);
     
     element1.appendChild(newText);
     
