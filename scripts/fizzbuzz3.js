96f683bfc2473b6f26e0e0b8ec42172acbaa6137
gman21788 <69609207+gman21788@users.noreply.github.com>
2021-11-09 21:28:02 -0500

    Update fizzbuzz3.js

--- scripts/fizzbuzz3.js
+++ scripts/fizzbuzz3.js
@@ -9,17 +9,25 @@ function getVars() {
 	fizzBuzz(firstDivisor, secondDivisor, thirdDivisor);	
 	}
 
-function fizzBuzz(firstDivisor, secondDivisor) {
+function fizzBuzz(firstDivisor, secondDivisor, thirdDivisor) {
    	for(let i = 1; i <= 140; i++) {
      		var word = '';  
-     		if (i % (firstDivisor * secondDivisor) === 0) {
-       		word = word.concat('HotHEAVY');
-     		} else if (i % firstDivisor === 0) {
-       		word = word.concat('Hot');
-     		} else if (i % secondDivisor === 0) {
-       		word = word.concat('HEAVY');
-		} else {
-       		word = word.concat('Order Crabs!');
+     		if (i % (firstDivisor * secondDivisor * thirdDivisor) === 0) {
+       			word = word.concat('HotHEAVYcRaBs!');
+     		} else if (i % firstDivisor * secondDivisor === 0) {
+       			word = word.concat('Hot');
+     		} else if (i % firstDivisor * thirdDivisor === 0) {
+       			word = word.concat('Heavy');
+		} else if {i % secondDivisor * thirdDivisor === 0) {
+			word = word.concat('Salty');
+		} else if {i % firstDivisor === 0) {
+			word = word.concat('Spicy');		
+		} else if {i % secondDivisor === 0) {
+			word = word.concat('Seafood-ey');		
+		} else if {i % thirdDivisor === 0) {
+			word = word.concat('Sandy');		
+		else {
+       			word = word.concat('Order Crabs!');
      	}
 	var element1 = document.createElement('li');
 	var newText = document.createTextNode(' ' + word);   
