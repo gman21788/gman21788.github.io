239ea961f228764da6f8ebf227a0b4e53ab6d7a5
gman21788 <69609207+gman21788@users.noreply.github.com>
2021-11-09 21:42:49 -0500

    Update fizzbuzz3.js

--- scripts/fizzbuzz3.js
+++ scripts/fizzbuzz3.js
@@ -4,31 +4,30 @@ function getVars() {
 	let lastName = document.getElementById('lastName').value;
 	let firstDivisor = 3;
 	let secondDivisor = 5;
-	let thirdDivisor = 7;
 	document.getElementById('greeting').textContent = 'Welcome to the Crab Shack, ' + firstName + ' ' + middleInit + ' ' + lastName + '!';
-	fizzBuzz(firstDivisor, secondDivisor, thirdDivisor);	
+	fizzBuzz(firstDivisor, secondDivisor);	
 	}
 
-function fizzBuzz(firstDivisor, secondDivisor, thirdDivisor) {
+function isDivisible(firstDivisor, secondDivisor) {
+	if (firstDivisor % secondDivisor === 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function fizzBuzz(firstDivisor, secondDivisor) {
    	for(let i = 1; i <= 140; i++) {
      		var word = '';  
-     		if (i % (firstDivisor * secondDivisor * thirdDivisor) === 0) {
-       		word = word.concat('HotHEAVYcRaBs!');
-     		} else if (i % firstDivisor * secondDivisor === 0) {
+     		if (i % (firstDivisor * secondDivisor) === 0) {
+       		word = word.concat('HotHEAVY');
+     		} else if (i % firstDivisor === 0) {
        		word = word.concat('Hot');
-     		} else if (i % firstDivisor * thirdDivisor === 0) {
-       		word = word.concat('Heavy');
-			} else if (i % secondDivisor * thirdDivisor === 0) {
-			word = word.concat('Salty');
-			} else if (i % firstDivisor === 0) {
-			word = word.concat('Spicy');		
-			} else if (i % secondDivisor === 0) {
-			word = word.concat('Seafood-ey');		
-			} else if (i % thirdDivisor === 0) {
-			word = word.concat('Sandy');		
-			else {
+     		} else if (i % secondDivisor === 0) {
+       		word = word.concat('HEAVY');
+		} else {
        		word = word.concat('Order Crabs!');
-     		}
+     	}
 	var element1 = document.createElement('li');
 	var newText = document.createTextNode(' ' + word);   
 	element1.appendChild(newText);    
